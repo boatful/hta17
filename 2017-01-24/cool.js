87b4ae97646a7f9ad020b3377febebaced8dad42
boatful <boatful@comcast.net>
2017-03-18 21:58:33 -0400

    Word Doc hep.docm now has working macrobuttons
    2 of them, a "Refresh" and a "Print", both working as expected.

---
+++ 2017-01-24/cool.js
@@ -0,0 +1,27 @@
+var cool_formats = {
+	"simple list": {
+		"opener":"",
+		"joiner":", ",
+		"closer":""
+	},
+	"run on list": {
+		"opener":"",
+		"joiner":" ",
+		"closer":""
+	},
+	"severity list": {
+		"opener":"",
+		"joiner":"-",
+		"closer":" out of 10"
+	},
+	"sentences": {
+		"opener":"",
+		"joiner":". ",
+		"closer":"."
+	},
+	"min per session": {
+		"opener":"",
+		"joiner":"-",
+		"closer":" minutes per session"
+	}
+}
---
+++ 2017-02-05/cool.js
@@ -0,0 +1,37 @@
+var cool_formats = {
+	"simple list": {
+		"opener":"",
+		"joiner":", ",
+		"closer":""
+	},
+	"run on list": {
+		"opener":"",
+		"joiner":" ",
+		"closer":""
+	},
+	"severity list": {
+		"opener":"",
+		"joiner":"-",
+		"closer":" out of 10"
+	},
+	"sentences": {
+		"opener":"",
+		"joiner":". ",
+		"closer":"."
+	},
+	"min per session": {
+		"opener":"",
+		"joiner":"-",
+		"closer":" minutes per session"
+	},
+	"bullets": {
+		"opener":"<ul><li>",
+		"joiner":"</li><li>",
+		"closer":"</li></ul>"
+	},
+	"topics": {
+		"opener":"<ul><li>Topic: ",
+		"joiner":" ***</li><li>Topic: ",
+		"closer":" ***</li></ul>"
+	}
+}
